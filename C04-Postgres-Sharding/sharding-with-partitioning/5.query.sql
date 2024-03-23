c2744a7c13f9812bb73058d31205be73556e7189
Prof TCSmith <smith515@usf.edu>
2024-03-23 09:54:14 -0400

    remove c04

--- C04-Postgres-Sharding/sharding-with-partitioning/5.query.sql
+++
@@ -1,6 +0,0 @@
--- uncomment one, as later selects will overwrite the screen output
-SELECT * FROM temperatures WHERE reading_date BETWEEN '2019-01-01' AND '2023-12-31';
-
---UPDATE temperatures SET temp = 90 WHERE reading_date='2019-06-15';
-
---SELECT * FROM temperatures WHERE reading_date BETWEEN '2019-01-01' AND '2023-12-31';

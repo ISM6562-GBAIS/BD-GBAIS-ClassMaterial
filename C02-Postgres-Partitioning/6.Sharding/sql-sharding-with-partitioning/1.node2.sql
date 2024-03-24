4ac8641b7327eda1aff05393267f560971aef683
Prof TCSmith <smith515@usf.edu>
2024-03-24 16:18:20 -0400

    update sql for partitioning and sharding

--- C02-Postgres-Partitioning/6.Sharding/sql-sharding-with-partitioning/1.node2.sql
+++ C02-Postgres-Partitioning/6.Sharding/sql-sharding-with-partitioning/1.node2.sql
@@ -1,17 +1,21 @@
+-- NOTE: If you are rerunning this SQL script, and node1_schema exists on node one you must first drop that
+-- schema on node1. Failure to do this will result in the following script seemingly runs OK, but the
+-- tables will not be created. 
+
+
 -- create a schema for our project
-CREATE SCHEMA IF NOT EXISTS node2_schema;
-SET search_path TO node2_schema;
+DROP SCHEMA IF EXISTS sharding_schema CASCADE;
+CREATE SCHEMA sharding_schema;
+SET search_path = sharding_schema;
 
-DROP TABLE IF EXISTS temps_2023, temps_2021;
-
-CREATE TABLE IF NOT EXISTS temps_2023 (
-    reading_date date PRIMARY KEY,
+CREATE TABLE temps_2023 (
+    reading_date date,
     city VARCHAR(100),
-    temp DECIMAL
+    temperature DECIMAL
 );
 
-CREATE TABLE IF NOT EXISTS temps_2021 (
-    reading_date date PRIMARY KEY,
+CREATE TABLE temps_2021 (
+    reading_date date,
     city VARCHAR(100),
-    temp DECIMAL
+    temperature DECIMAL
 );
